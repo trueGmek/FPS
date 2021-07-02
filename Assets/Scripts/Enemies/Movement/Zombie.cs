1712d7bdf7a74dc4dde12fbf28a8943b0a34a751
trueGmek <gmek0204@gmail.com>
2021-07-02 12:02:31 +0200

    Introduce a moving enemy
    *Rename materials
    *Add two new textures
    *Change the testScene
    *Rename the CubeBody class to TestBodyClass
    *Bake a NavMesh for the testScene

---
+++ Assets/Scripts/Enemies/Movement/Zombie.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemies.Movement {
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Zombie : MonoBehaviour {
+        public Transform player;
+        private NavMeshAgent _agent;
+
+        private void Start() {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update() {
+            _agent.destination = player.transform.position;
+        }
+    }
+}
